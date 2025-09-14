47877f855d024d078c60803056e100d6011be936
barb4433 <161386762+barb4433@users.noreply.github.com>
2025-09-14 20:23:51 +0000

    lab 2

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -2,13 +2,11 @@
 #include <string>
 using namespace std; // making life easier so that we do not need to use std::cin , std::cout, etc.
 
-
 //Write the function called locateSmallest in the given interval such that 
 // output : int: index of the smallest value in the array
 // input : an array of int, the starting index of search, the ending point of searchspace
 // function: find the location (i.e index) of the smallest value of array in the given interval
 // Note that I don't want the smallest value
-
 
 int locateSmallest(int array[], int start, int end)
 {
@@ -40,22 +38,16 @@ void replaceVariable(int array[], int value, int start, int end)
 	}
 }
 
-
-
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
 void printArray(int array[], int size)
 {
-	return;
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << endl;
+	}
 }
-
-
-
-
-
-
-
 
 //Write a function called sumOdds in the given interval such that 
 // output : integer
@@ -64,7 +56,15 @@ void printArray(int array[], int size)
 //Hint: % is used for modulo operation. 
 //     n%m gives you the remainder after dividing n to m. for example 7%3 = 1 because 7 = 3*2 + 1, then 1 is the remainder.
 
-
-int sumOdds(int array[], int start, int end){
-	return -1;
+int sumOdds(int array[], int start, int end)
+{
+	int sum = 0;
+	for (int i = start; i <= end; i++)
+	{
+		if (array[i] % 2 != 0)
+		{
+			sum += array[i];
+		}
+	}
+	return sum;
 }
